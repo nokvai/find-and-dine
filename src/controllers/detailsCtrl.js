fbb8dab396afad27c2b784fd9e850fc573c6c568
nokvai <bmeliseo@gmail.com>
2019-05-28 20:53:00 +0800

    Temp push 3

--- src/controllers/detailsCtrl.js
+++ src/controllers/detailsCtrl.js
@@ -1,4 +1,5 @@
 import style from '../sass/details.scss';
+import moment from 'moment';
 
 export default class DetailsCtrl {
   constructor($stateParams, businessService) {
@@ -6,14 +7,35 @@ export default class DetailsCtrl {
     this.style = style;
     this.businessService = businessService;
     this.stateParam = $stateParams;
-    this.detail = {};
+    this.detail = [];
+    this.start = '';
+    this.end = '';
   }
 
   $onInit() {
     this.businessService.view(this.stateParam.id).then((res) => {
-      this.detail = res;
-      console.log(this.detail);
+        this.detail = res;
+        this.loadGoogleMap(this.detail.coordinates.latitude, this.detail.coordinates.longitude);
     });
   }
 
+  formatTime(time) {
+    return  moment(time.substring(0, 2).concat(':').concat(time.substring(2, 4)), 'hh:mm a').format('HH:mm A'); 
+  }
+  formatDay(n) {
+   return ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'][n];
+  }
+
+  loadGoogleMap(long, lat) {
+    const maps = new google.maps.Map(document.getElementById('googlemap'), {
+        zoom: 18,
+        center: { lat: long, lng: lat }
+    });
+
+    new google.maps.Marker({
+        position: new google.maps.LatLng(long, lat),
+        map: maps,
+        title: this.detail.name
+    });
+  }
 }

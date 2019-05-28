2878c58f4db9fb597b355c2c4694d2a91ce647cd
nokvai <bmeliseo@gmail.com>
2019-05-28 23:59:22 +0800

    Task 4 Commit 5

--- src/controllers/detailsCtrl.js
+++ src/controllers/detailsCtrl.js
@@ -10,17 +10,34 @@ export default class DetailsCtrl {
     this.detail = [];
     this.start = '';
     this.end = '';
+    this.containerPhoto = [];
+    this.indicator = 0;
   }
 
   $onInit() {
     this.businessService.view(this.stateParam.id).then((res) => {
         this.detail = res;
+        console.log('this.detail:',  this.detail);
         this.loadGoogleMap(this.detail.coordinates.latitude, this.detail.coordinates.longitude);
+        this.loadImages(this.indicator);
     });
+    window.scrollTo(0, 0);
+  }
+
+  loadImages(i) { 
+    this.indicator = i;
+    var photo2 = this.detail.photos[i + 1]; 
+    
+    if (this.detail.photos) {
+      if ((this.detail.photos.length - 1) === i) { 
+          photo2 = this.detail.photos[0];
+      }
+      this.containerPhoto = [this.detail.photos[i], photo2];
+    }
   }
 
   formatTime(time) {
-    return  moment(time.substring(0, 2).concat(':').concat(time.substring(2, 4)), 'hh:mm a').format('HH:mm A'); 
+    return  moment(time.substring(0, 2).concat(':').concat(time.substring(2, 4)), 'hh:mm a').format('hh:mm A'); 
   }
   formatDay(n) {
    return ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'][n];
